f9600fb9d1e9fb954f7e5dd7cda4343041a0ce69
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-11-17 20:32:22 -0300

    feat: setup inicial

---
+++ src/main/java/br/senac/go/music/MusicProjectApplication.java
@@ -0,0 +1,13 @@
+package br.senac.go.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicProjectApplication.class, args);
+	}
+
+}
