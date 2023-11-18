4c831c8b6e61788e336b108540c4464db40aed2a
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-11-18 15:40:55 -0300

    feat: explicacao jpa

--- src/main/java/br/senac/go/music/relationship/onetoone/User.java
+++ src/main/java/br/senac/go/music/relationship/onetoone/User.java
@@ -1,6 +1,8 @@
 package br.senac.go.music.relationship.onetoone;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -10,7 +12,18 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private Integer id;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "NAME", nullable = false, length = 100)
+    private String name;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "EMAIL", nullable = false, length = 100)
+    private String email;
 
     @OneToOne(cascade = CascadeType.ALL)
     //A diretiva joincolumn está relacionada ao vínculo dos registros (ids) via banco de dados
