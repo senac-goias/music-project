4975c4280f42249f1350884bbf3e69764eaed288
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-11-17 21:09:11 -0300

    doc: inserida documentação do projeto

--- src/main/java/br/senac/go/music/model/Musica.java
+++ src/main/java/br/senac/go/music/model/Musica.java
@@ -1,4 +1,25 @@
 package br.senac.go.music.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data //lombok
+@Entity //jpa
+@Table (name = "musica") // jpa
 public class Musica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(
+            name = "NM_MUSICA",
+            length = 100,
+            unique = true,
+            nullable = false,
+            columnDefinition = "VARCHAR(45)",
+            insertable = true,
+            updatable = true
+            /*table = "outra_tabela"*/)
+    private String nome;
 }
